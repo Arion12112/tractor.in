1ed1f808de29251900521b94aafe0a4865d4a2a0
Aaron1111 <25971010+Aaron1111@users.noreply.github.com>
2019-10-02 21:20:18 +0700

    qna page

--- src/app/qn-a/qn-a.page.ts
+++ src/app/qn-a/qn-a.page.ts
@@ -5,11 +5,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './qn-a.page.html',
   styleUrls: ['./qn-a.page.scss'],
 })
-export class QnAPage implements OnInit {
+export class QnAPage {
 
-  constructor() { }
+  items;
 
-  ngOnInit() {
+  constructor() {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.items = [
+      {name: 'Sarah', question: 'Bagaimana cara merawat traktor?', like : '6', comments : '7', log: '8'},
+      {name: 'Andi', question: 'Apakah traktor bajak singkal cocok untuk lahan rawa?',like : '12', comments : '134', log: '4'},
+      {name: 'Bagus', question: 'Apakah traktor rotari bisa digunakan untuk membajak semua jenis lahan?',like : '200', comments : '1021', log: '12'}
+      
+      
+    ];
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.name.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
+
