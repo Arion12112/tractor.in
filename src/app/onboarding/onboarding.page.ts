6bd16931185e4e34953dd47ded30da9e34b73bd2
Aaron1111 <25971010+Aaron1111@users.noreply.github.com>
2019-10-02 12:15:17 +0700

    halaman edukasi
    education page css

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { delay } from 'q';
 
 @Component({
   selector: 'app-onboarding',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardingPage implements OnInit {
 
-  constructor() { }
+  constructor(private splashScreen: SplashScreen) {
+    this.splashScreen.show();
+    delay(5000);
+    this.splashScreen.hide();
+   }
 
   ngOnInit() {
   }
+  
 
 }
