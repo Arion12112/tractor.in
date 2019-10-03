0626cf4acb88261b20116e8b8d514a022e3520ac
minggitprakasa <inggit_prakasa@apps.ipb.ac.id>
2019-10-03 17:28:52 +0700

    ja

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-=======
-
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-     path: 'tabs',
-     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-
-  {path : '', loadChildren: './onboarding/onboarding.module#OnboardingPageModule'},
-  { path: 'monitor', loadChildren: './monitor/monitor.module#MonitorPageModule' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'calendar', loadChildren: './calendar/calendar.module#CalendarPageModule' },
-  { path: 'qn-a', loadChildren: './qn-a/qn-a.module#QnAPageModule' },
-  { path: 'education', loadChildren: './education/education.module#EducationPageModule' },
-  { path: 'education-video', loadChildren: './education-video/education-video.module#EducationVideoPageModule' },
-  { path: 'insight1', loadChildren: './insight1/insight1.module#Insight1PageModule' },
-  { path: 'onboarding', loadChildren: './onboarding/onboarding.module#OnboardingPageModule' },
-  { path: 'profiles', loadChildren: './profiles/profiles.module#ProfilesPageModule' },
-  { path: 'service', loadChildren: './service/service.module#ServicePageModule' },
-  { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
-  { path: 'olah-tanah', loadChildren: './insight1/olah-tanah/olah-tanah.module#OlahTanahPageModule' },
-  { path: 'irigasi', loadChildren: './insight1/irigasi/irigasi.module#IrigasiPageModule' },
-  { path: 'tanam', loadChildren: './insight1/tanam/tanam.module#TanamPageModule' },
-  { path: 'panen', loadChildren: './insight1/panen/panen.module#PanenPageModule' },
-  { path: 'barter', loadChildren: './barter/barter.module#BarterPageModule' },  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
->>>>>>> 418c30d738fc4e30ddda64dfbd473121c565292d
-
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -57,7 +25,8 @@ const routes: Routes = [
   { path: 'panen', loadChildren: './insight1/panen/panen.module#PanenPageModule' },
   { path: 'barter', loadChildren: './barter/barter.module#BarterPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'alsintan-olah', loadChildren: './AnalisisAlsintan/alsintan-olah-tanah/alsintan-olah-tanah.module#AlsintanOlahTanahPageModule' }
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'alsintan-olah', loadChildren: './AnalisisAlsintan/alsintan-olah-tanah/alsintan-olah-tanah.module#AlsintanOlahTanahPageModule' },
 
 ];
 @NgModule({
