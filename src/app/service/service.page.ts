4ea1175392592a3fcae2103a238b918c4188c256
Aaron1111 <25971010+Aaron1111@users.noreply.github.com>
2019-10-03 15:33:00 +0700

    calendar

--- src/app/service/service.page.ts
+++ src/app/service/service.page.ts
@@ -5,11 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './service.page.html',
   styleUrls: ['./service.page.scss'],
 })
-export class ServicePage implements OnInit {
+export class ServicePage {
 
-  constructor() { }
+  items;
+  jaraks;
 
-  ngOnInit() {
+  constructor() {
+    this.initializeItems();
   }
 
+  initializeItems() {
+    this.items = [
+      'Bengkel Sindojaya',
+      'Bengkel Majucerah',
+      'Bengkel Tigacahaya'
+    ];
+ 
+  }
+  
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  
+
 }
+
