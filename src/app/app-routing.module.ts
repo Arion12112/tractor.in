4c97c29194cc496ec8a483c01b8f1d8dd7cb639b
minggitprakasa <inggit_prakasa@apps.ipb.ac.id>
2019-10-03 15:31:45 +0700

    Tambahan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,9 @@
-<<<<<<< HEAD
+
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-
-  { path: 'monitor', loadChildren: './monitor/monitor.module#MonitorPageModule' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'calendar', loadChildren: './calendar/calendar.module#CalendarPageModule' },
-  { path: 'qn-a', loadChildren: './qn-a/qn-a.module#QnAPageModule' },
-  { path: 'education', loadChildren: './education/education.module#EducationPageModule' },
-  { path: 'education-video', loadChildren: './education-video/education-video.module#EducationVideoPageModule' },
-  { path: 'insight1', loadChildren: './insight1/insight1.module#Insight1PageModule' },
-  { path: 'onboarding', loadChildren: './onboarding/onboarding.module#OnboardingPageModule' },
-  { path: 'profiles', loadChildren: './profiles/profiles.module#ProfilesPageModule' },
-  { path: 'service', loadChildren: './service/service.module#ServicePageModule' },
-  { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
-  { path: 'olah-tanah', loadChildren: './insight1/olah-tanah/olah-tanah.module#OlahTanahPageModule' },
-  { path: 'irigasi', loadChildren: './insight1/irigasi/irigasi.module#IrigasiPageModule' },
-  { path: 'tanam', loadChildren: './insight1/tanam/tanam.module#TanamPageModule' },
-  { path: 'panen', loadChildren: './insight1/panen/panen.module#PanenPageModule' },  { path: 'barter', loadChildren: './barter/barter.module#BarterPageModule' },
-  { path: 'alsintan-olah-tanah', loadChildren: './AnalisisAlsintan/alsintan-olah-tanah/alsintan-olah-tanah.module#AlsintanOlahTanahPageModule' },
-  { path: 'analisis-irigasi', loadChildren: './AnalisisAlsintan/analisis-irigasi/analisis-irigasi.module#AnalisisIrigasiPageModule' },
-  { path: 'analisis-tanam', loadChildren: './AnalisisAlsintan/analisis-tanam/analisis-tanam.module#AnalisisTanamPageModule' },
-  { path: 'analisis-panen', loadChildren: './AnalisisAlsintan/analisis-panen/analisis-panen.module#AnalisisPanenPageModule' }
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
->>>>>>> 00749abcd38843ae5a5e99d44d6f907a21b51f2e
-
-const routes: Routes = [
-  {
-  
      path: 'tabs',
      loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
