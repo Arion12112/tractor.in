c253b4df997b870b171b3cd10e72a5e3d361bafc
Aaron1111 <25971010+Aaron1111@users.noreply.github.com>
2019-10-03 16:13:59 +0700

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,8 @@ const routes: Routes = [
   { path: 'irigasi', loadChildren: './insight1/irigasi/irigasi.module#IrigasiPageModule' },
   { path: 'tanam', loadChildren: './insight1/tanam/tanam.module#TanamPageModule' },
   { path: 'panen', loadChildren: './insight1/panen/panen.module#PanenPageModule' },
-  { path: 'barter', loadChildren: './barter/barter.module#BarterPageModule' }
+  { path: 'barter', loadChildren: './barter/barter.module#BarterPageModule' },  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+
 
 
 ];
