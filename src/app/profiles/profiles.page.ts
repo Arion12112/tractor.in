3fd54b25ec5604dff128437354d40508c20b6e8d
Aaron1111 <25971010+Aaron1111@users.noreply.github.com>
2019-10-03 20:35:12 +0700

    profil

--- src/app/profiles/profiles.page.ts
+++ src/app/profiles/profiles.page.ts
@@ -10,53 +10,5 @@ import { User, AuthService } from '../auth-service.service';
 })
 export class ProfilesPage {
 
-  saveSuccess = false;
-  info: User;
-
-  constructor( private router: Router,
-    private auth: AuthService, private alertCtrl: AlertController, 
-    private loading: LoadingController ) { 
-
-      if( this.auth.checkLogged() ) {
-        this.info = this.auth.getUserInfo(); 
-      } else {
-        console.log('not logged');
-        this.router.navigateByUrl('/login');
-      }
-  }
-
-  public save() {
-    this.auth.register(this.info).subscribe(success => {
-      if (success) {
-        this.saveSuccess = true;
-        this.showPopup("Success", "Thanks! Profile updated.");
-      } else {
-        this.showPopup("Error", "Problem updating profile.");
-      }
-    },
-    error => {
-      this.showPopup("Error", error);
-    });
-  }
-
-  async showPopup(title, text) {
-    let alert = await this.alertCtrl.create({
-      header: title,
-      message: text,
-      buttons: [
-        {
-          text: 'OK',
-          handler: data => {
-            if (this.saveSuccess) {
-              // console.log('create successs');
-              this.router.navigateByUrl('/home');
-            }
-          }
-        }
-      ]
-    });
-
-    return await alert.present();
-  }
 
 }
